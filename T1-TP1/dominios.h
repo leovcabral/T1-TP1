237405202288580da0b8b21ef07d96b8c4850e8f
arthur <arthur.lacava01@gmail.com>
2022-02-25 18:56:47 -0300

    Terminando documentação dos dominios

--- T1-TP1/dominios.h
+++ T1-TP1/dominios.h
@@ -11,6 +11,15 @@
 
 using namespace std;
 
+
+/// Domínio Idioma.
+///
+/// Campo da entidade Sessão.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~.md
+///Tem valor definido que deve estar na lista de idiomas definidos.
+///Inglês, Chinês Mandarim, Hindi, Espanhol, Francês, Árabe, Bengali, Russo, Português,
+///Indonésio
+///~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Idioma
 {
 
@@ -18,15 +27,28 @@ private:
 
     static const int TAM_DISP = 10;
     string disponivel[TAM_DISP] = {"Ingles", "Chines Mandarim", "Hindi", "Espanhol", "Frances", "Arabe", "Bengali", "Russo", "Portugues", "Indonesio"};
-    string selecionado = "";
+    string selecionado;
     void validar(string str);
 
 public:
-
+    /// Função que imprime todas as linguagens disponíveis.
     void getDisp();
+    /// Funcão para definir valor da classe Idioma.
+    /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, é definido como valor de Idioma para a instancia.
+    /// @param str String que deve ser definida como valor para Idioma da instancia.
     void setIdioma(string str);
     string getIdioma();
 };
+
+/// Domínio Endereço.
+///
+/// Campo da entidade Excursão.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~.md
+/// Requisitos do valor:
+///  0 a 20 caracteres.
+///  Não há espaços em branco em sequência.
+///  Não há pontos (.) em sequência.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class Endereco
 {
@@ -36,11 +58,25 @@ private:
     void validar(string str);
 
 public:
-
+    /// Funcão para definir valor da classe Endereço.
+    /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, é definido como valor de Endereço para a instancia.
+    /// @param str String que deve ser definida como valor para Endereço da instancia.
     void setEndereco(string str);
     string getEndereco();
 };
 
+
+
+/// Domínio Data.
+///
+/// Campo da entidade Sessão.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~.md
+/// Requisitos do valor:
+///   Tem formato DD-MES-AAAA
+///   DD - 01 a 31
+///   MES -Jan, Fev, Mar, Abr, Mai, Jun, Jul, Ago, Set, Out, Nov, Dez
+///   AA - 2000 a 9999
+///~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Data
 {
 
@@ -54,12 +90,26 @@ private:
 
 
 public:
-
+    /// Funcão para definir valores da classe Data.
+    /// Valida os argumentos conforme os requisitos da classe. Se os argumentos forem validos, eles são definidos como valores que compoẽm o valor de data para a instancia.
+    /// @param DD Inteiro que deve ser definido como valor de Dia para instancia da classe Data.
+    /// @param Mes String que deve ser definida como valor para Mes da instancia.
+    /// @param AA Inteiro que deve ser definido como valor de Ano para instancia da classe Data.
     void setData(const int DD, string, const int AA);
+    ///@returns String com valor da instancia da classe Data com formato DD-Mes-AAAA.
     string getData();
 
 };
 
+/// Domínio Horário.
+///
+/// Campo da entidade Sessão.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~.md
+/// Requisitos do valor:
+///   Tem formato HH:MM
+///   HH - 00 a 23
+///   MM - 00 a 59
+///~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Horario
 {
 
@@ -70,13 +120,36 @@ private:
     void validar(int HH, int MM);
 
 public:
-
+    /// Funcão para definir valores de horas e minutos da classe Horário.
+    /// Valida os argumentos conforme os requisitos da classe. Se os argumentos forem validos, eles são definidos como valores de horas e minutos para a instancia.
+    /// @param HH Inteiro que deve ser definido como valor de Hora para instancia da classe Horário.
+    /// @param MM Inteiro que deve ser definido como valor de Minutos para instancia da classe Horário.
     void setHorario(int HH, int MM);
+    ///@returns String com valor da instancia da classe Horário com formato HH:MM.
     string getHorario();
 
 
 };
 
+/// Domínio Email.
+///
+/// Campo da entidade Usuário.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~.md
+/// Requisitos do valor:
+///    Formato parte-local@domínio
+///    parte-local é composta por até 64 caracteres.
+///    Caractere de parte local pode ser letra maiúscula (A-Z) ou minúscula (a-z).
+///    Caractere de parte local pode ser dígito (0-9).
+///    Caractere de parte local pode ser ! # $ % & ' * + - / = ? ^ _ ` { | } ~
+///    Caractere de parte local pode ser ponto (.) desde que não seja o primeiro ou o último
+///    caractere e que não ocorra em sequência.
+///    domínio é composto por até 253 caracteres.
+///    Caractere de domínio pode ser letra maiúscula (A-Z) ou minúscula (a-z).
+///    Caractere de domínio pode ser dígito (0-9).
+///    Caractere de domínio pode hífen (-).
+///    Caractere de domínio pode ser ponto (.) desde que não seja o primeiro caractere e não ocorra
+///    em sequência.
+///~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Email {
 
     private:
@@ -86,8 +159,11 @@ class Email {
 
 
     public:
-
+    /// Funcão para definir valor da classe Email.
+    /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
+    /// @param valor String que deve ser definida como valor da instancia da classe Email.
     void setEmail(string str);
+    ///@returns String com valor da instancia da classe Email.
     string getEmail();
 
 
@@ -113,7 +189,7 @@ class Cidade {
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
         /// @param valor String que deve ser definida como cidade da instancia.
         void setCidade(string);
-        ///@returns string com valor da instancia da classe cidade.
+        ///@returns String com valor da instancia da classe cidade.
         string getCidade() const;
 };
 
@@ -136,7 +212,7 @@ class Nota {
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
          /// @param valor Inteiro que deve ser definido como Nota da instancia.
         void setValor(int);
-        ///@returns inteiro com valor da instancia da classe Nota.
+        ///@returns Inteiro com valor da instancia da classe Nota.
         int getValor() const;
 };
 
@@ -161,9 +237,9 @@ class Descricao  {
     public:
         /// Funcão para definir valor da classe Descrição.
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
-         /// @param valor String que deve ser definida como valor da instancia da classe Descrição.
+        /// @param valor String que deve ser definida como valor da instancia da classe Descrição.
         void setDescription(string);
-        ///@returns string com valor da instancia da classe Descrição.
+        ///@returns String com valor da instancia da classe Descrição.
         string getDescription() const;
 };
 
@@ -191,7 +267,7 @@ class Titulo {
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
         /// @param valor String que deve ser definida como valor da instancia da classe Título.
         void setTitulo(string);
-        ///@returns string com valor da instancia da classe Título.
+        ///@returns String com valor da instancia da classe Título.
         string getTitulo() const;
 };
 
@@ -199,7 +275,7 @@ inline string Titulo::getTitulo() const {
     return valor;
 }
 
-/// Domínio Senha.
+///Domínio Senha.
 ///
 ///Campo da entidade Usuário.
 ///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.md
@@ -220,7 +296,7 @@ class Senha {
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
         /// @param valor String que deve ser definida como valor da instancia da classe Senha.
         void setPassword(string);
-        ///@returns string com valor da instancia da classe Senha.
+        ///@returns String com valor da instancia da classe Senha.
         string getPassword() const;
 };
 
@@ -240,7 +316,7 @@ class Nome {
         /// @param valor String que deve ser definida como valor da instancia da classe Nome.
         void setTitulo(string);
         void setValor(string);
-        ///@returns string com valor da instancia da classe Nome.
+        ///@returns String com valor da instancia da classe Nome.
         string getValor() const;
 };
 
@@ -266,7 +342,7 @@ class Duracao {
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
          /// @param valor Inteiro que deve ser definido como Nota da Duração.
         void setValor(int);
-        ///@returns inteiro com valor da instancia da classe Duração.
+        ///@returns Inteiro com valor da instancia da classe Duração.
         int getValor() const;
 };
 
@@ -291,7 +367,7 @@ class Codigo {
         /// Valida o argumento conforme os requisitos da classe. Se o argumento for valido, o argumento passado é definido como valor para a instancia.
         /// @param valor String que deve ser definida como valor da instancia da classe Código.
         void setValor(string);
-        ///@returns string com valor da instancia da classe Código.
+        ///@returns String com valor da instancia da classe Código.
         string getValor() const;
 };
 
