dbeec97144b55f8d60287fa823423fe93cbccdb0
arthur <arthur.lacava01@gmail.com>
2022-04-16 23:23:25 -0300

    base apresentacao autenticacao

--- T1-TP1/interfaces.h
+++ T1-TP1/interfaces.h
@@ -14,8 +14,8 @@
 // Forward declarations.
 
 class IServicoAutenticacao;
-class IServicoPessoal;
-class IServicoProdutosFinanceiros;
+//class IServicoPessoal;
+//class IServicoProdutosFinanceiros;
 
 //--------------------------------------------------------------------------------------------
 // Declarações das interfaces da camada de apresentação.
@@ -24,29 +24,30 @@ class IServicoProdutosFinanceiros;
 // 200053523
 class IApresentacaoAutenticacao {
     public:
-        virtual bool autenticar(Usuario*) = 0;
+        virtual bool autenticar(Email*) = 0;
         virtual void setCntrServicoAutenticacao(IServicoAutenticacao*) = 0;
         virtual ~IApresentacaoAutenticacao(){}
 };
 
-// 190046953
-class IApresentacaoConta{
-    public:
-        virtual void cadastrar() = 0;
-        virtual void editar(Usuario*) = 0;
-        virtual void descadastrar(Usuario*) = 0;
-        virtual void setCntrServicoConta(IServicoConta*) = 0;
-        virtual ~IApresentacaoConta(){}
-};
-
-// 202061421
-class IApresentacaoExcursao{
-    public:
-        virtual void executar() = 0;
-        virtual void executar(Email) = 0;
-        virtual void setCntrServicosExcursao(IServicoExcursao*) = 0;
-        virtual ~IApresentacaoExcursao(){}
-};
+//// 190046953
+//class IApresentacaoConta{
+//    public:
+//        virtual void executar(Email) = 0;
+//        virtual void cadastrar() = 0;
+////        virtual void editar(Email*) = 0;
+////        virtual void descadastrar(Email*) = 0;
+//        virtual void setCntrServicoConta(IServicoConta*) = 0;
+//        virtual ~IApresentacaoConta(){}
+//};
+//
+//// 202061421
+//class IApresentacaoExcursao{
+//    public:
+//        virtual void executar() = 0;
+//        virtual void executar(Email) = 0;
+//        virtual void setCntrServicosExcursao(IServicoExcursao*) = 0;
+//        virtual ~IApresentacaoExcursao(){}
+//};
 
 //--------------------------------------------------------------------------------------------
 // Declarações das interfaces da camada de serviço.
@@ -57,23 +58,23 @@ class IServicoAutenticacao {
         virtual ~IServicoAutenticacao(){}
 };
 
-class IServicoConta{
-public:
-        virtual bool cadastrarConta(Usuario*) = 0;
-        virtual ~IServicoConta(){}
-};
-
-class IServicoExcursao{
-public:
-        virtual void listar() = 0;
-        virtual bool cadastrar(Usuario*) = 0
-        virtual bool descadastrar(Usuario*) = 0;
-        virtual bool avaliar(Usuario*, Excursao*) = 0;
-        virtual bool acessarExcursao(Excursao*,Usuario*) = 0;
-        virtual bool acessarAvaliacao(Excursao*, Usuario*) = 0;
-        virtual bool acessarSessao(Excursao*, Usuario*) = 0;
-        virtual bool setCntrServicosExcursao(IServicoExcursao*) = 0;                      // Adaptar aos requisitos.
-        virtual ~IServicoExcursao(){}
-};
+//class IServicoConta{
+//public:
+//        virtual bool cadastrarConta(Usuario*) = 0;
+//        virtual ~IServicoConta(){}
+//};
+//
+//class IServicoExcursao{
+//public:
+//        virtual void listar() = 0;
+//        virtual bool cadastrar(Usuario*) = 0
+//        virtual bool descadastrar(Usuario*) = 0;
+//        virtual bool avaliar(Usuario*, Excursao*) = 0;
+//        virtual bool acessarExcursao(Excursao*,Usuario*) = 0;
+//        virtual bool acessarAvaliacao(Excursao*, Usuario*) = 0;
+//        virtual bool acessarSessao(Excursao*, Usuario*) = 0;
+//        virtual bool setCntrServicosExcursao(IServicoExcursao*) = 0;                      // Adaptar aos requisitos.
+//        virtual ~IServicoExcursao(){}
+//};
 
 #endif // INTERFACES_H_INCLUDED
---
+++ T1-TP1/stubs.cpp
@@ -0,0 +1,80 @@
+#include "stubs.h"
+
+//--------------------------------------------------------------------------------------------
+// Valores usados como gatilhos de erros.
+
+const string StubServicoAutenticacao::EMAIL_VALIDO = "nando@email.com";
+const string StubServicoAutenticacao::SENHA_VALIDO = "Ab123456";
+//const string StubServicoPessoal::INVALIDO = "12345";
+//const string StubServicoProdutosFinanceiros::INVALIDO = "12345";
+
+//--------------------------------------------------------------------------------------------
+// Implementa��es dos m�todos de classes stub.
+
+bool StubServicoAutenticacao::autenticar(Email email, Senha senha){
+    if(email.getEmail().compare(EMAIL_VALIDO) == 0 &&
+        senha.getPassword().compare(SENHA_VALIDO) == 0){
+        return true;
+        }
+    else{
+        return false;
+        }
+}
+//
+//bool StubServicoPessoal::cadastrarUsuario(Usuario usuario){
+//    if(usuario.getCPF().getValor().compare(INVALIDO) == 0)
+//        return false;
+//    return true;
+//}
+//
+//bool StubServicoProdutosFinanceiros::cadastrarConta(Conta conta){
+//    if(conta.getNumero().getValor().compare(INVALIDO) == 0)
+//        return false;
+//    return true;
+//}
+//
+//bool StubServicoProdutosFinanceiros::consultarConta(Conta *conta){
+//    if(conta->getNumero().getValor().compare(INVALIDO) == 0)
+//        return false;
+
+    //--------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------
+    // Implementar c�digo que atribui valores ao objeto identificado por conta.
+    //--------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------
+//
+//    return true;
+//}
+
+//bool StubServicoProdutosFinanceiros::cadastrarProdutoInvestimento(Produto produto){
+//    if(produto.getCodigo().getValor().compare(INVALIDO) == 0)
+//        return false;
+//    return true;
+//}
+//
+//bool StubServicoProdutosFinanceiros::descadastrarProdutoInvestimento(Codigo codigo){
+//    if(codigo.getValor().compare(INVALIDO) == 0)
+//        return false;
+//    return true;
+//}
+//
+//bool StubServicoProdutosFinanceiros::realizarAplicacao(Aplicacao aplicacao){
+//    if(aplicacao.getCodigo().getValor().compare(INVALIDO) == 0)
+//        return false;
+//    return true;
+//}
+//
+//bool StubServicoProdutosFinanceiros::recuperarAplicacao(Aplicacao *aplicacao){
+//    if(aplicacao->getCodigo().getValor().compare(INVALIDO) == 0)
+//        return false;
+//
+//    //--------------------------------------------------------------------------------------------
+//    //--------------------------------------------------------------------------------------------
+//    // Implementar c�digo que atribui valores ao objeto identificado por aplicacao.
+//    //--------------------------------------------------------------------------------------------
+//    //--------------------------------------------------------------------------------------------
+//
+//    return true;
+//}
+//
+
